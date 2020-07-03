e8203a9decfe60f0bf3e238fed9a148d20a31396
Ayush Ray <i.ayushray95@gmail.com>
2020-07-03 00:15:12 -0700

    Initial push of major project

---
+++ MajorProject/login/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = "login"
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('home/', views.home_view, name='home'),
+]
